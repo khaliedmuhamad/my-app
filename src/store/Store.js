4aaf66b53ab615fda38ceb47009170f6243411e1
khaliedmuhamad <khaled195348@feng.bu.edu.eg>
2021-05-04 03:10:39 +0200

    Updates

--- src/store/Store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import reducers from './reducers/reducers.js';
-
-    let store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-
-export default store;
--- src/store/types.js
+++
@@ -1,5 +0,0 @@
-export  const ADD_TO_FAV = 'ADD_TO_FAV' ;
-
-export  const REMOVE_FROM_FAV = 'REMOVE_FROM_FAV' ;
-
-
